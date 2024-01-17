e003c8ad8da60e61db076ed1a8d07703744fec74
Philip Kim <filshouse56@gmail.com>
2024-01-17 14:29:02 -0500

    Exercise Completed

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ console.log(
 );
 
 // Exercise 1
+const firstname = "Philip";
+let lastName;
+var age;
 
 // Exercise 2
 
+lastName = "Kim"
+age = 43
+
 // Exercise 3
+
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+// Exercise 5
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookMark = 456
+let hasRead = true
+
+
+console.log(`${title} written by ${author} is ${pageCount} pages long. 
+The last bookmark is on page ${bookMark}.`)
